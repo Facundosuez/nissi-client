2b45f1d13ca095e05518f80469d830802000287b
facundo suez <facundosuez2003@gmail.com>
2023-11-04 02:26:25 -0300

    nissi pagina

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Modal from './components/global/Modal'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <Modal />
+  )
+}
+
+export default App
